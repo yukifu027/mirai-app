e9cce2b627f8b0c1fe46e1f707243b07e8b57317
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-05-31 14:59:39 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root "places#index"
   resources :places do
     resources :comments, only: [:new, :edit, :create, :update, :destroy]
