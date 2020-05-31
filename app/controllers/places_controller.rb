91db5e9f497e8d0834d301d7e4b11bc7d7c868e1
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-05-31 10:28:57 +0900

    詳細画面でのインクリメンタルサーチ

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,5 +12,11 @@ class PlacesController < ApplicationController
   def show 
     @place = Place.find(params[:id])
     @comments = @place.comments.includes(:user)
+    return nil if params[:keyword] == ""
+    @places_key = Place.where(['name LIKE ?', "%#{params[:keyword]}%"] ).limit(5)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 end
