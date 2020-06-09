9b116ae8b46b90f5c1324bfca5f1ce4f2fdcb61c
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-06-09 21:48:50 +0900

    遺産とコメントのバリデーション追加

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,6 +1,6 @@
 class Place < ApplicationRecord
   has_many :comments
   belongs_to :user
-
+  validates :name, :image, :content, :webpage, presence: true
   mount_uploader :image, ImageUploader
 end
