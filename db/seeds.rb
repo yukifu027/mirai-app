5f03431de75305e21b40c5d18e28671f1e8a8a1b
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-06-09 10:35:33 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,6 @@
-# User.create!(
-#   name: '奇跡の一本松',
-#   image: File.open('public/places/clouds-871718_1280.jpg'),
-#   content: '高田松原７万本のうち唯一生き残った復興のシンボル。 震災後枯死しましたが、陸前高田市が寄付金により現状の形で再整備しました。',
-#   webpage: 'https://www.city.rikuzentakata.iwate.jp/kategorie/fukkou/ipponmatu/ipponmatu.html'
-
-# )
-
 Place.create!(
   name: '奇跡の一本松',
-  image: File.open('public/places/clouds-871718_1280.jpg'),
+  image: File.open('places/clouds-871718_1280.jpg'),
   content: '高田松原７万本のうち唯一生き残った復興のシンボル。 震災後枯死しましたが、陸前高田市が寄付金により現状の形で再整備しました。',
   webpage: 'https://www.city.rikuzentakata.iwate.jp/kategorie/fukkou/ipponmatu/ipponmatu.html',
   user_id: "1"
