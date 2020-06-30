2a4720da5a5dc98471895ecf00ef2f9c85b7f19c
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-06-30 15:53:00 +0900

    SNS認証に必要なモデル、コントローラを用意

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   validates :name, presence: true, uniqueness: true
   has_many :comments
   has_many :places
+  has_many :sns_credentials
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
