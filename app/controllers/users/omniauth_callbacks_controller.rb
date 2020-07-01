3a0f529ef3947a22f88ea42f7c7310d45fa67a90
yukifu027 <63824425+yukifu027@users.noreply.github.com>
2020-07-01 10:44:42 +0900

    SNSログイン

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    authorization
+  end
+
+  def google_oauth2
+    authorization
+  end
+
+  def failure
+    redirect_to root_path
+  end
+
+  private
+
+  def authorization
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted? #ユーザー情報が登録済みなので、新規登録ではなくログイン処理を行う
+      sign_in_and_redirect @user, event: :authentication
+    else #ユーザー情報が未登録なので、新規登録画面へ遷移する
+      render template: 'devise/registrations/new'
+      # render action: :new
+    end
+  end
+end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,6 +13,8 @@ Devise.setup do |config|
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
+  config.omniauth :facebook,ENV['FACEBOOK_CLIENT_ID'],ENV['FACEBOOK_CLIENT_SECRET']
+  config.omniauth :google_oauth2,ENV['GOOGLE_CLIENT_ID'],ENV['GOOGLE_CLIENT_SECRET']
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
